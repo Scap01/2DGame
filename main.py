380adb5584cae054485f9daafd4bcc85fa286b49
Scap <picwin12@gmail.com>
2021-08-27 15:10:32 -0300

    window setting set

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import pygame
+pygame.init()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#creating game window
+pygame.display.set_mode((800, 600))
+pygame.display.set_caption("2dg - beta")
 
+#game loop
+running = True
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pygame.quit()
